4ee48c3ee3e7f7c55be79a795a3a567990881032
atanas prodanov <atanasprodanov69@gmail.com>
2025-08-15 14:39:20 +0300

    Database update and migration for ProcedureCategory

---
+++ Infrastructure/Migrations/20250815102754_Optional_Procedure_Category.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Optional_Procedure_Category : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Procedures_ProcedureCategories_ProcedureCategoryId",
+                table: "Procedures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcedureCategoryId",
+                table: "Procedures",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Procedures_ProcedureCategories_ProcedureCategoryId",
+                table: "Procedures",
+                column: "ProcedureCategoryId",
+                principalTable: "ProcedureCategories",
+                principalColumn: "ProcedureCategoryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Procedures_ProcedureCategories_ProcedureCategoryId",
+                table: "Procedures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcedureCategoryId",
+                table: "Procedures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Procedures_ProcedureCategories_ProcedureCategoryId",
+                table: "Procedures",
+                column: "ProcedureCategoryId",
+                principalTable: "ProcedureCategories",
+                principalColumn: "ProcedureCategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Infrastructure/Persistance/Entities/Procedure.cs
+++ Infrastructure/Persistance/Entities/Procedure.cs
@@ -6,8 +6,8 @@
         public string ProcedureName { get; set; } = string.Empty;
         public int Duration { get; set; }
         public decimal Price { get; set; }
-        public int ProcedureCategoryId { get; set; }
-        public ProcedureCategory ProcedureCategory { get; set; } = null!;
+        public int? ProcedureCategoryId { get; set; }
+        public ProcedureCategory? ProcedureCategory { get; set; } = null!;
         public List<AppointmentProcedure> AppointmentProcedures { get; set; } = [];
     }
 }

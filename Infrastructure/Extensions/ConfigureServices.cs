de4243d3524c52d578b8471f995e2be1a9716e69
atanas prodanov <atanasprodanov69@gmail.com>
2025-08-21 15:34:10 +0300

    Initial identity setup

--- Infrastructure/Extensions/ConfigureServices.cs
+++ Infrastructure/Extensions/ConfigureServices.cs
@@ -1,4 +1,6 @@
 ﻿using Infrastructure.Persistance;
+using Infrastructure.Persistance.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,8 +11,31 @@ namespace Infrastructure.Extensions
     {
         public static void AddInfrstructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext();
+
+            services.AddIdentity();
+        }
+
+        public static void AddDbContext(this IServiceCollection services)
+        {
             services.AddDbContext<GaneaDbContext>(opt =>
                 opt.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION")));
+        }
+
+        public static void AddIdentity(this IServiceCollection services)
+        {
+            services.AddIdentityCore<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<GaneaDbContext>()
+                .AddDefaultTokenProviders();
         }
     }
 }
--- Infrastructure/Migrations/20250821104558_Adding_AspNetCore_Identity.Designer.cs
+++ Infrastructure/Migrations/20250821104558_Adding_AspNetCore_Identity.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(GaneaDbContext))]
-    partial class GaneaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250821104558_Adding_AspNetCore_Identity")]
+    partial class Adding_AspNetCore_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,11 +102,11 @@ namespace Infrastructure.Migrations
 
             modelBuilder.Entity("Infrastructure.Persistance.Entities.Employee", b =>
                 {
-                    b.Property<int>("EmployeeId")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("EmployeeId"));
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
                     b.Property<string>("FirstName")
                         .IsRequired()
@@ -115,14 +118,7 @@ namespace Infrastructure.Migrations
                         .HasMaxLength(20)
                         .HasColumnType("nvarchar(20)");
 
-                    b.Property<string>("UserId")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(450)");
-
-                    b.HasKey("EmployeeId");
-
-                    b.HasIndex("UserId")
-                        .IsUnique();
+                    b.HasKey("Id");
 
                     b.ToTable("Employees");
                 });
@@ -411,17 +407,6 @@ namespace Infrastructure.Migrations
                     b.Navigation("Procedure");
                 });
 
-            modelBuilder.Entity("Infrastructure.Persistance.Entities.Employee", b =>
-                {
-                    b.HasOne("Infrastructure.Persistance.Entities.User", "User")
-                        .WithOne("Employee")
-                        .HasForeignKey("Infrastructure.Persistance.Entities.Employee", "UserId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("User");
-                });
-
             modelBuilder.Entity("Infrastructure.Persistance.Entities.Procedure", b =>
                 {
                     b.HasOne("Infrastructure.Persistance.Entities.ProcedureCategory", "ProcedureCategory")
@@ -505,12 +490,6 @@ namespace Infrastructure.Migrations
             modelBuilder.Entity("Infrastructure.Persistance.Entities.ProcedureCategory", b =>
                 {
                     b.Navigation("Procedures");
-                });
-
-            modelBuilder.Entity("Infrastructure.Persistance.Entities.User", b =>
-                {
-                    b.Navigation("Employee")
-                        .IsRequired();
                 });
 #pragma warning restore 612, 618
         }

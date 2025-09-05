d576dca7511f448dcda183b2e9c5a7f21c5fd829
atanas prodanov <atanasprodanov69@gmail.com>
2025-09-05 15:33:58 +0300

    commiting hehehe

--- Application/Common/Models/User/UserModel.cs
+++ Application/Common/Models/User/UserModel.cs
@@ -9,15 +9,12 @@ namespace Infrastructure.Common.Models
 {
     public class UserModel
     {
-        [EmailAddress]
         public string Email { get; set; } = null!;
 
         public string UserName { get; set; } = null!;
 
-        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; } = null!;
 
-        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; } = null!;
     }
 }
